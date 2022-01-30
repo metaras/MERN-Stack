2abd35225f086df3dae4dfda27b9c16b2deb728e
Metaras <metaras.c@ku.th>
2022-01-30 22:56:32 +0700

    add realm

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/restaurants",
+  baseURL: "https://ap-southeast-1.aws.data.mongodb-api.com/app/restaurantsreviews-amsob/endpoint/",
+  // baseURL: "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-reviews-evuay/service/restaurants/incoming_webhook/",
   headers: {
     "Content-type": "application/json"
   }
